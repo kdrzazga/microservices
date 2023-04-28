80db6bfd214c18be5cb83f6306880da329442e6f
jerzy <jerzy.z.bialowiezy@proton.me>
2023-04-28 10:03:07 +0200

    Adding HTTP methods

--- main/core/card.py
+++ main/core/card.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 import yaml
-from loguru import logger
-from flask import Flask, jsonify, json, Response
+from flask import Flask, jsonify, json, Response, render_template
 from flask_httpauth import HTTPBasicAuth
+from loguru import logger
 
 SERVICE_NAME = 'CARD'
 
@@ -10,6 +12,8 @@ app = Flask(__name__)
 users = {
     "admin": "admin"
 }
+
+recently_deleted = {}
 
 basic_auth = HTTPBasicAuth()
 
@@ -20,12 +24,19 @@ def verify_password(username, password):
 
 
 @app.route('/', methods=['GET'])
-def say_hello() -> str:
+def serve_page():
     logger.info("Hello from service " + SERVICE_NAME)
-    return 'Microservice %s' % SERVICE_NAME
+    return render_template('card_request.html')
 
 
-@app.route('/<card_type>/<card_id>', methods=['GET'])
+@app.route('/read/<card_id>', methods=['GET'])
+def read_card_old() -> Response:
+    message = "Info about cards available at /<card_type>/<card_id>"
+    logger.warning(message)
+    return jsonify(message), 308, {'Content-Type': 'application/json'}  # permanent redirect
+
+
+@app.route('/read/<card_type>/<card_id>', methods=['GET'])
 @basic_auth.login_required
 def read_card(card_type: str, card_id: int) -> Response:
     logger.info("Request for " + card_type + " [" + str(card_id) + "]")
@@ -36,7 +47,60 @@ def read_card(card_type: str, card_id: int) -> Response:
     selected_cards = cards['credit-card'] if card_type == 'credit' else cards['debit-card']
     logger.info("Card data:\n%s", json.dumps(selected_cards, indent=2))
 
-    return jsonify(_get_card_by_id(selected_cards, card_id))
+    return jsonify(_get_card_by_id(selected_cards, card_id)), 200, {'Content-Type': 'application/json'}  # OK
+
+
+@app.route('/create/<name>/<customer_name>/<account_ref>', methods=['POST'])
+@basic_auth.login_required
+def create_credit_card(name, customer_name, account_ref, issue_date=None, file_path='cards.yml') -> Response:
+    new_id = create_credit_card_service(name, account_ref, customer_name, file_path, issue_date)
+
+    return jsonify('Card created id=' + str(new_id)), 201, {'Content-Type': 'application/json'}
+
+
+def create_credit_card_service(name, account_ref, customer_name, file_path='cards.yml', issue_date=None):
+    # Load the YAML file
+    with open(file_path, 'r') as file:
+        data = yaml.load(file, Loader=yaml.FullLoader)
+
+    # Find the highest existing ID in the credit card list
+    existing_ids = [card['id'] for card in data['credit-card']]
+    new_id = max(existing_ids) + 1 if existing_ids else 1
+
+    # Set a default issue date of the current date and time
+    if issue_date is None:
+        issue_date = datetime.now().strftime('%Y-%m-%d')
+
+    # Create a new credit card dictionary with the assigned ID and default issue date
+    new_card = {'id': new_id,
+                'name': name,
+                'customer-name': customer_name,
+                'issue-date': issue_date,
+                'account-ref': account_ref}
+
+    # Add the new card to the credit card list
+    data['credit-card'].append(new_card)
+
+    # Save the updated YAML file
+    with open(file_path, 'w') as file:
+        yaml.dump(data, file)
+    return new_id
+
+
+def delete_credit_card_service(card_id, card_type='credit-card', file_path='cards.yml'):
+    # Load the YAML file
+    with open(file_path, 'r') as file:
+        data = yaml.load(file, Loader=yaml.FullLoader)
+
+    for record in data[card_type]:
+        if record['id'] == card_id:
+            data[card_type].remove(record)
+            break  # stop searching after the first match
+
+    # Save the updated YAML file
+    with open(file_path, 'w') as file:
+        yaml.dump(data, file)
+    return True
 
 
 def _get_card_by_id(cards, card_id: int):
---
+++ test/core/CardTest.py
@@ -0,0 +1,30 @@
+import yaml
+import unittest
+from datetime import datetime
+from loguru import logger
+
+from main.core.card import create_credit_card_service, delete_credit_card_service
+
+
+class CardTest(unittest.TestCase):
+    def test_create_credit_card(self):
+        now = datetime.now().strftime('%Y-%m-%d')
+        file_path = '../../main/core/cards.yml'
+        new_id = create_credit_card_service('Visa', 'Ian Tester', 3456, file_path, now)
+
+        with open(file_path, 'r') as file:
+            data = yaml.load(file, Loader=yaml.FullLoader)
+
+        # Find the highest existing ID in the credit card list
+        card_type = 'credit-card'
+        existing_ids = [card['id'] for card in data[card_type]]
+
+        self.assertIn(new_id, existing_ids)
+        logger.info("Created " + card_type + " with id = " + str(new_id))
+        logger.info("Deleting " + str(new_id) + "...")
+        # clean up
+        delete_credit_card_service(new_id, card_type, file_path)
+
+
+if __name__ == '__main__':
+    unittest.main()
